3d0fb08e69ef61f434238f08e58f9dea9c56903c
pavitpim <pavitpim@gmail.com>
2020-06-11 18:16:21 +0700

    Start Fetch

--- 7.Node.js/Content_Part1/test-pm-all.js
+++ 7.Node.js/Content_Part1/test-pm-all.js
@@ -1,2 +1,29 @@
 // Promise all 
 
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function pmRandom () {
+    return new Promise( (rs,rj) => {
+        let n = getRandom(1,9)
+        if (n < 7) rs(n)
+        else rj(n)
+    })
+}
+
+const pm1 = pmRandom() 
+const pm2 = pmRandom() 
+const pm3 = pmRandom() 
+
+
+Promise.all([ pm1, pm2, pm3])
+.then( rs => {
+    console.log("Pass all");
+    console.log(rs);
+}).catch( rj => {
+    console.log("Not Pass");
+    console.log(rj);
+} )
+
+

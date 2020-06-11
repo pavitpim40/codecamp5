998d27a8af36b5eff53b0cae2512ba9df5fffb58
pavitpim <pavitpim@gmail.com>
2020-06-11 17:04:14 +0700

    Node 11/6

--- 7.Node.js/Content_Part1/test12.75.js
+++ 7.Node.js/Content_Part1/test12.75.js
@@ -10,22 +10,19 @@ return new Promise ( (resolve,reject) => {
 }
 
  
-async function runGetFile(){
+(async () => {
 try {
-    const data = await getFile('./start.txt');
+    let data = await getFile('./start.txt')
     console.log(data);
-
-    const d1 = await getFile(data)
-    console.log(d1);
-
-    const d2 = await getFile(d1)
-    console.log(d2);
-    
-    const d3 = await getFile(d2)
-    console.log(d3);
+    data = await getFile(data)
+    console.log(data);
+    data = await getFile(data)
+    console.log(data);
+    data = await getFile(data)
+    console.log(data);    
 }catch (err) {
     console.log(err);
 }
-} 
+}) ()
 
-runGetFile()
+// runGetFile()

76c9cdc06ffb0fc84a9de3091f42002c4ac6b381
pavitpim <pavitpim@gmail.com>
2020-06-11 23:10:01 +0700

    Finish JSON

---
+++ 7.Node.js/Content_Part2/lab01.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+let data = fs.readFileSync('./sample.json' , 'utf-8')
+//console.log(data);
+
+let jsonArr = JSON.parse(data).users
+//console.log(jsonArr);
+//console.log(typeof(jsonArr))
+
+jsonArr.map ( x => {
+    if (x.lastName === 'mac') x.lastName = 'Apple'
+})
+//console.log(jsonArr);
+
+// แปลงกลับเป็น JSON
+let newdata = JSON.stringify(jsonArr)
+//console.log(newdata);
+fs.writeFileSync ('./lab01_output.json',newdata)
+//console.log('Save output.json');
